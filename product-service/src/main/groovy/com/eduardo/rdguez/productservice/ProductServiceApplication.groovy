0665ae743a6071843df0b1b0146450c2d062a79d
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-07-10 15:44:37 -0500

    coding feature/MSB-1 Building a product service project

---
+++ product-service/src/main/groovy/com/eduardo/rdguez/productservice/ProductServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.eduardo.rdguez.productservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProductServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication, args)
+	}
+
+}
---
+++ product-service/src/test/groovy/com/eduardo/rdguez/productservice/ProductServiceApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.eduardo.rdguez.productservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
