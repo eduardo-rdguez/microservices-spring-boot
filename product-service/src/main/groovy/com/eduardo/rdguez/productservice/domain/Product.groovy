3e8c5dbb2387ed26bc5fdfa302069fe6a44bf195
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-07-12 08:52:00 -0500

    coding feature/MSB-1 Add product domain

---
+++ product-service/src/main/groovy/com/eduardo/rdguez/productservice/domain/Product.groovy
@@ -0,0 +1,17 @@
+package com.eduardo.rdguez.productservice.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id
+  String name
+  Double price
+
+}
