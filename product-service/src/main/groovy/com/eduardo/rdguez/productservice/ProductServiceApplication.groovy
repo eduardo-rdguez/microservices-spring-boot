c12bcce082347ac0bf7a2468776ffe5d9e177303
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-07-15 22:29:09 -0500

    coding feature/MSB-7 Add eureka-client configuration to product-service

--- product-service/src/main/groovy/com/eduardo/rdguez/productservice/ProductServiceApplication.groovy
+++ product-service/src/main/groovy/com/eduardo/rdguez/productservice/ProductServiceApplication.groovy
@@ -2,12 +2,14 @@ package com.eduardo.rdguez.productservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class ProductServiceApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication, args)
-	}
+  static void main(String[] args) {
+    SpringApplication.run(ProductServiceApplication, args)
+  }
 
 }
