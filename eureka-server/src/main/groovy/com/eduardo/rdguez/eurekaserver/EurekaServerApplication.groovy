473e26b29906a456688d231ba640bcead5bac215
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-07-15 21:59:24 -0500

    coding feature/MSB-6 Building eureka server

---
+++ eureka-server/src/main/groovy/com/eduardo/rdguez/eurekaserver/EurekaServerApplication.groovy
@@ -0,0 +1,15 @@
+package com.eduardo.rdguez.eurekaserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaServerApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication, args)
+  }
+
+}
---
+++ eureka-server/src/test/groovy/com/eduardo/rdguez/eurekaserver/EurekaServerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.eduardo.rdguez.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
